89acbd3beb32e6890d89c8007deffc8fac300df2
netor27 <neto.r27@gmail.com>
2017-12-05 21:10:13 -0600

    Adding SqlAlchemy config file

---
+++ api/config.py
@@ -0,0 +1,12 @@
+import os
+
+
+# You need to replace the next values with the appropriate values for your configuration
+basedir = os.path.abspath(os.path.dirname(__file__))
+DEBUG = True
+PORT = 5000
+HOST = "127.0.0.1"
+SQLALCHEMY_ECHO = False
+SQLALCHEMY_TRACK_MODIFICATIONS = True
+SQLALCHEMY_DATABASE_URI = "postgresql://{DB_USER}:{DB_PASS}@{DB_ADDR}/{DB_NAME}".format(DB_USER="user_name", DB_PASS="password", DB_ADDR="127.0.0.1", DB_NAME="messages")
+SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
