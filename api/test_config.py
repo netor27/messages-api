b6390f09e705a7d0c399ea7d0aa2f31a6a164b95
netor27 <neto.r27@gmail.com>
2018-01-05 22:13:31 -0600

    adding test config file

--- api/test_config.py
+++ api/test_config.py
@@ -13,7 +13,7 @@ PORT = 5000
 HOST = "127.0.0.1"
 SQLALCHEMY_ECHO = False
 SQLALCHEMY_TRACK_MODIFICATIONS = True
-SQLALCHEMY_DATABASE_URI = "postgresql://{DB_USER}:{DB_PASS}@{DB_ADDR}/{DB_NAME}".format(DB_USER="user_name", DB_PASS="password", DB_ADDR="127.0.0.1", DB_NAME="test_messages")
+SQLALCHEMY_DATABASE_URI = "postgresql://{DB_USER}:{DB_PASS}@{DB_ADDR}/{DB_NAME}".format(DB_USER="user_name", DB_PASS="password", DB_ADDR="127.0.0.1", DB_NAME="YOUR_TEST_DB_NAME")
 SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
 TESTING = True
 SERVER_NAME = '127.0.0.1:5000'
