6e8f19f0ef84e9b31a440e5f42c4dc610e1c91da
netor27 <neto.r27@gmail.com>
2018-01-05 21:45:04 -0600

    Adding basic authentication to api resources

--- api/models.py
+++ api/models.py
@@ -2,6 +2,8 @@ from marshmallow import Schema, fields, pre_load
 from marshmallow import validate
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
+from passlib.apps import custom_app_context as password_context
+import re
 
 
 db = SQLAlchemy()
@@ -19,6 +21,36 @@ class AddUpdateDelete():
     def delete(self, resource):
         db.session.delete(resource)
         return db.session.commit()
+
+
+class User(db.Model, AddUpdateDelete):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), unique=True, nullable=False)
+    # I save the hashed password
+    hashed_password = db.Column(db.String(120), nullable=False)
+    creation_date = db.Column(db.TIMESTAMP, server_default=db.func.current_timestamp(), nullable=False)
+
+    def verify_password(self, password):
+        return password_context.verify(password, self.hashed_password)
+
+    def check_password_strength_and_hash_if_ok(self, password):
+        if len(password) < 8:
+            return 'The password is too short', False
+        if len(password) > 32:
+            return 'The password is too long', False
+        if re.search(r'[A-Z]', password) is None:
+            return 'The password must include at least one uppercase letter', False
+        if re.search(r'[a-z]', password) is None:
+            return 'The password must include at least one lowercase letter', False
+        if re.search(r'\d', password) is None:
+            return 'The password must include at least one number', False
+        if re.search(r"[ !#$%&'()*+,-./[\\\]^_`{|}~"+r'"]', password) is None:
+            return 'The password must include at least one symbol', False
+        self.hashed_password = password_context.encrypt(password)
+        return '', True
+
+    def __init__(self, name):
+        self.name = name
 
 
 class Message(db.Model, AddUpdateDelete):
@@ -99,3 +131,7 @@ class MessageSchema(ma.Schema):
         return data
 
 
+class UserSchema(ma.Schema):
+    id = fields.Integer(dump_only=True)
+    name = fields.String(required=True, validate=validate.Length(3))
+    url = ma.URLFor('api.userresource', id='<id>', _external=True)
--- api/views.py
+++ api/views.py
@@ -4,15 +4,84 @@ from models import db, Category, CategorySchema, Message, MessageSchema
 from sqlalchemy.exc import SQLAlchemyError
 import status
 from helpers import PaginationHelper
+from flask_httpauth import HTTPBasicAuth
+from flask import g
+from models import User, UserSchema
+
+
+auth = HTTPBasicAuth()
+
+
+@auth.verify_password
+def verify_user_password(name, password):
+    user = User.query.filter_by(name=name).first()
+    if not user or not user.verify_password(password):
+        return False
+    g.user = user
+    return True
+
+
+class AuthRequiredResource(Resource):
+    method_decorators = [auth.login_required]
 
 
 api_bp = Blueprint('api', __name__)
 category_schema = CategorySchema()
 message_schema = MessageSchema()
+user_schema = UserSchema()
 api = Api(api_bp)
 
 
-class MessageResource(Resource):
+class UserResource(AuthRequiredResource):
+    def get(self, id):
+        user = User.query.get_or_404(id)
+        result = user_schema.dump(user).data
+        return result
+
+
+class UserListResource(Resource):
+    @auth.login_required
+    def get(self):
+        pagination_helper = PaginationHelper(
+            request,
+            query=User.query,
+            resource_for_url='api.userlistresource',
+            key_name='results',
+            schema=user_schema)
+        result = pagination_helper.paginate_query()
+        return result
+
+    def post(self):
+        request_dict = request.get_json()
+        if not request_dict:
+            response = {'user': 'No input data provided'}
+            return response, status.HTTP_400_BAD_REQUEST
+        errors = user_schema.validate(request_dict)
+        if errors:
+            return errors, status.HTTP_400_BAD_REQUEST
+        name = request_dict['name']
+        existing_user = User.query.filter_by(name=name).first()
+        if existing_user is not None:
+            response = {'user': 'An user with the same name already exists'}
+            return response, status.HTTP_400_BAD_REQUEST
+        try:
+            user = User(name=name)
+            error_message, password_ok = \
+                user.check_password_strength_and_hash_if_ok(request_dict['password'])
+            if password_ok:
+                user.add(user)
+                query = User.query.get(user.id)
+                result = user_schema.dump(query).data
+                return result, status.HTTP_201_CREATED
+            else:
+                return {"error": error_message}, status.HTTP_400_BAD_REQUEST
+        except SQLAlchemyError as e:
+            db.session.rollback()
+            resp = {"error": str(e)}
+            return resp, status.HTTP_400_BAD_REQUEST
+
+
+class MessageResource(AuthRequiredResource):
     def get(self, id):
         message = Message.query.get_or_404(id)
         result = message_schema.dump(message).data
@@ -60,7 +129,7 @@ class MessageResource(Resource):
                 return resp, status.HTTP_401_UNAUTHORIZED
 
 
-class MessageListResource(Resource):
+class MessageListResource(AuthRequiredResource):
     def get(self):
         pagination_helper = PaginationHelper(
             request,
@@ -106,7 +175,7 @@ class MessageListResource(Resource):
             return resp, status.HTTP_400_BAD_REQUEST
 
 
-class CategoryResource(Resource):
+class CategoryResource(AuthRequiredResource):
     def get(self, id):
         category = Category.query.get_or_404(id)
         result = category_schema.dump(category).data
@@ -148,7 +217,7 @@ class CategoryResource(Resource):
                 return resp, status.HTTP_401_UNAUTHORIZED
 
 
-class CategoryListResource(Resource):
+class CategoryListResource(AuthRequiredResource):
     def get(self):
         categories = Category.query.all()
         results = category_schema.dump(categories, many=True).data
@@ -182,3 +251,5 @@ api.add_resource(CategoryListResource, '/categories/')
 api.add_resource(CategoryResource, '/categories/<int:id>')
 api.add_resource(MessageListResource, '/messages/')
 api.add_resource(MessageResource, '/messages/<int:id>')
+api.add_resource(UserListResource, '/users/')
+api.add_resource(UserResource, '/users/<int:id>')
