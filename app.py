4cb5918e7d5bd899611bf27080dc019b116f464a
Ernesto Rubio Fuentes <neto.r27@gmail.com>
2018-01-17 17:43:48 -0600

    Adding arguments to set config file

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
+import sys
 from web import create_app
 
-app = create_app("config", debug=True)
+configFile = "config"
+if len(sys.argv) > 1:
+    configFile = sys.argv[1]
+
+app = create_app(configFile, debug=True)
 
 if __name__ == '__main__':    
     app.run(host='0.0.0.0', port=5000, debug=True)
