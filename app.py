607f9546f3409e584cd94a1cfcde59524d1d275c
Ernesto Rubio Fuentes <neto.r27@gmail.com>
2018-01-18 11:43:45 -0600

    Adding db upgrade at app startup

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import sys
 from web import create_app
+from web.models import db
 
 configFile = "config"
 if len(sys.argv) > 1:
@@ -7,5 +8,11 @@ if len(sys.argv) > 1:
 
 app = create_app(configFile, debug=True)
 
-if __name__ == '__main__':    
+if __name__ == '__main__':   
+    # Create in the db the tables that are missing
+    app_context = app.app_context()
+    app_context.push()
+    db.create_all()
+
+    # start the app
     app.run(host='0.0.0.0', port=5000, debug=True)
